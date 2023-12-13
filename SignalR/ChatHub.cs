c80edfc786e7c7a280c5fcc10e5693cfe38273c6
angryWilly <kirill.teyxrib@yandex.ru>
2023-12-14 04:28:40 +0700

    add ChatHub and map it

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR;
+
+public class ChatHub : Hub
+{
+    public async Task Send(string message)
+    {
+        await Clients.All.SendAsync("Receive", message);
+    }
+}
--- SignalR/Program.cs
+++ SignalR/Program.cs
@@ -1,6 +1,12 @@
+using SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
